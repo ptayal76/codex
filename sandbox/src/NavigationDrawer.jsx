863fa8fce058330dc4613c3033308101dfdeaf64
Pallav Pandey <pallav.pandey@thoughtspot.com>
2024-06-06 17:34:47 +0530

    updated icons

--- sandbox/src/NavigationDrawer.jsx
+++ sandbox/src/NavigationDrawer.jsx
@@ -22,53 +22,59 @@ const NavigationDrawer = ({ onTabChange }) => {
   // };
 
   return (
-    <div className="nav-drawer">
-      <div className="tabs flex flex-col p-2 gap-2">
-        <div
-          className={`tab ${activeTab === 'checkCluster' && 'active'}`}
-          onClick={() => handleTabClick('checkCluster')}
-        >
-          Check Cluster
-        </div>
-        <div
-          className={`tab ${activeTab === 'createCluster' && 'active'}`}
-          onClick={() => handleTabClick('createCluster')}
-        >
-          Create Cluster
-        </div>
-        <div
-          className={`tab ${activeTab === 'GrafanaLogs' && 'active'}`}
-          onClick={() => handleTabClick('GrafanaLogs')}
-        >
-          Grafana Logs
-        </div>
-        {isGrafanaLogsOpen && (
-          <div className="sub-tabs flex flex-col p-2 gap-2">
-            <div className="tab" onClick={() => handleTabClick('GrafanaLogs1')}>V2 Public Apis</div>
-            <div className="tab" onClick={() => handleTabClick('GrafanaLogs2')}>V1 Public Apis</div>
-            <div className="tab" onClick={() => handleTabClick('GrafanaLogs3')}>Internal Apis</div>
+      <div className="nav-drawer">
+        <img src="/static/icons/TS-logo-white-no-bg.svg" alt="TS" className={"ts-icon"}/>
+        <div className="tabs flex flex-col p-2 gap-2">
+          <div
+              className={`tab ${activeTab === 'checkCluster' && 'active'}`}
+              onClick={() => handleTabClick('checkCluster')}
+          >
+            <img src="/static/icons/settings-svgrepo-com.svg" alt="Check Cluster" className="tab-icon"/>
+            Check Cluster
           </div>
-        )}
-        <div
-          className={`tab ${activeTab === 'KibanaLogs' && 'active'}`}
-          onClick={() => handleTabClick('KibanaLogs')}
-        >
-          Kibana Logs
-        </div>
-        <div
-          className={`tab ${activeTab === 'Sandbox' && 'active'}`}
-          onClick={() => handleTabClick('Sandbox')}
-        >
-          Sandbox
-        </div>
-        <div
-          className={`tab ${activeTab === 'other' && 'active'}`}
-          onClick={() => handleTabClick('other')}
-        >
-          Other
+          <div
+              className={`tab ${activeTab === 'createCluster' && 'active'}`}
+              onClick={() => handleTabClick('createCluster')}
+          >
+            <img src="/static/icons/ic_cluster.svg" alt="Create Cluster" className="tab-icon"/>
+            Create Cluster
+          </div>
+          <div
+              className={`tab ${activeTab === 'GrafanaLogs' && 'active'}`}
+              onClick={() => handleTabClick('GrafanaLogs')}
+          >
+            <img src="/static/icons/grafana-icon.svg" alt="Grafana Logs" className="tab-icon"/>
+            Grafana Logs
+          </div>
+          {isGrafanaLogsOpen && (
+              <div className="sub-tabs flex flex-col p-2 gap-2">
+                <div className="tab" onClick={() => handleTabClick('GrafanaLogs1')}>V2 Public Apis</div>
+                <div className="tab" onClick={() => handleTabClick('GrafanaLogs2')}>V1 Public Apis</div>
+                <div className="tab" onClick={() => handleTabClick('GrafanaLogs3')}>Internal Apis</div>
+              </div>
+          )}
+          <div
+              className={`tab ${activeTab === 'KibanaLogs' && 'active'}`}
+              onClick={() => handleTabClick('KibanaLogs')}
+          >
+            <img src="/static/icons/kibana-svgrepo-com.svg" alt="Kibana" className="tab-icon"/>
+            Kibana Logs
+          </div>
+          <div
+              className={`tab ${activeTab === 'Sandbox' && 'active'}`}
+              onClick={() => handleTabClick('Sandbox')}
+          >
+            <img src="/static/icons/sandbox-svgrepo-com.svg" alt="Sandbox" className="tab-icon"/>
+            Sandbox
+          </div>
+          <div
+              className={`tab ${activeTab === 'other' && 'active'}`}
+              onClick={() => handleTabClick('other')}
+          >
+            Other
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
