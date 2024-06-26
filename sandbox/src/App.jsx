58dcd25342ffabbf2450c6ca38d253c3f8fbaeaa
piyush-tayal <piyush.tayal@thoughtspot.com>
2024-06-26 12:01:17 +0530

    remove redundancy

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,12 +1,5 @@
-import React, { useState } from "react";
-import FormComponent from "./CreateKBCluster.jsx";
-import GrafanaLogs from "./GrafanaLogs.jsx";
+import React from "react";
 import "./styles.css";
-import ClusterDetails from "./Clusterdetails.jsx";
-import Sandbox from "./Sanbox.jsx";
-import KibanaLogsContainer from "./KibanaLogsContainer/kibanaLogsContainer.jsx";
-import CheckConfigurations from "./checkConfigurations.jsx";
-import HarAnalyze from "./Har_analyse.jsx";
 import { SecureRoute, Security, LoginCallback } from "@okta/okta-react";
 import { OktaAuth } from "@okta/okta-auth-js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -14,9 +7,6 @@ import Home from "./Home.jsx";
 import LoginDashboard from "./Login.jsx";
 
 export default function App() {
-  const [results, setResults] = useState([]);
-  const [scrollableData, setScrollableData] = useState([]);
-  const [activeTab, setActiveTab] = useState("checkCluster");
   const restoreOriginalUri = async () => {
     window.location.href = "/home";
   };
