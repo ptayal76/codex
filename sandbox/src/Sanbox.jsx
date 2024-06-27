83a3bdb7589831837d77dde7d5d48a41ba9b462b
tanmypani <tanmay.pani@thoughtspot.com>
2024-06-27 12:00:54 +0530

    added new workflow to sandbox and also refactored the sandbox code to handle multiple workflows

--- sandbox/src/Sanbox.jsx
+++ sandbox/src/Sanbox.jsx
@@ -5,8 +5,9 @@ const Sandbox = () => {
   return (
     <div className="sandbox-container">
       <div className="iframe-wrapper">
+        {/* src="https://codesandbox.io/embed/vsrqqr?view=preview&module=%2Fsrc%2Fcomponents%2FLandingPage.tsx&hidenavigation=1" */}
         <iframe 
-          src="https://codesandbox.io/embed/vsrqqr?view=preview&module=%2Fsrc%2Fcomponents%2FLandingPage.tsx&hidenavigation=1"
+          src="https://codesandbox.io/embed/j4ch89?view=preview&module=%2Fsrc%2Fcomponents%2Fhome.tsx&hidenavigation=1"
           className="sandbox-iframe"
           title="tse"
           allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
